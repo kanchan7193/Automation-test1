73a493c61dae749ecffd1e303167ab28bb5b6b55
Kanchan <kmahajan331@gmail.com>
2020-08-16 16:27:01 +0200

    another selenium test

--- test1/src/test/java/test1/Spec.java
+++ test1/src/test/java/test1/Spec.java
@@ -4,6 +4,13 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 /**
  * Unit test for simple App.
  */
@@ -15,6 +22,20 @@ public class Spec
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( false );
+        assertTrue( true );
+    }
+
+    @Test
+    public void testgoogle(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.get("https://google.com/ncr");
+            driver.findElement(By.name("q")).sendKeys("cheese" + Keys.ENTER);
+            WebElement firstResult = driver.findElement(By.cssSelector("h3>div"));
+            System.out.println(firstResult.getAttribute("textContent"));
+        } finally {
+            driver.quit();
+        }
     }
 }
